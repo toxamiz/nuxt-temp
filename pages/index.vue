d1de81059f3ed427b989e115389087b5bba4d66c
Anton <38910496+toxamiz@users.noreply.github.com>
2019-11-08 17:41:59 +0300

    init

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="l-container">
+        hello
+    </div>
+</template>
+
+<script>
+    import {LightProbeGenerator} from 'three/examples/jsm/lights/LightProbeGenerator'
+    export default {
+        mounted() {
+            console.log(LightProbeGenerator);
+        }
+    }
+</script>
